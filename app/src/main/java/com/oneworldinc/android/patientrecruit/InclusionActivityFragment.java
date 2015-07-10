ba774a7e87b4a7419b9b71376771fa8c8bcf3b14
s.thavasukkannu <s.thavasukannu@gmail.com>
2015-07-11 01:39:32 +0530

    Updated by KBala

--- app/src/main/java/com/oneworldinc/android/patientrecruit/InclusionActivityFragment.java
+++ app/src/main/java/com/oneworldinc/android/patientrecruit/InclusionActivityFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -28,10 +29,16 @@ View rootView;
         rootView= inflater.inflate(R.layout.fragment_inclusion, container, false);
         int exclusiveContentPosition = 0;
         RadioGroup radioGroup = (RadioGroup) rootView.findViewById(R.id.inclusive_radiogroup);
-        final TextView exclusivePass = (TextView) rootView.findViewById(R.id.inclusive_pass);
-        final TextView exclusiveContent = (TextView) rootView.findViewById(R.id.inclusive_text);
-        final String[] exclusiveContent_array = getResources().getStringArray(R.array.inclusion_criteria);
-        exclusiveContent.setText(exclusiveContent_array[0]);
+        final TextView inclusionPass = (TextView) rootView.findViewById(R.id.inclusive_pass);
+        final TextView inclusionContent = (TextView) rootView.findViewById(R.id.inclusive_text);
+        final String[] inclusionContent_array = getResources().getStringArray(R.array.inclusion_criteria);
+        final LinearLayout alertLayout = (LinearLayout) rootView.findViewById(R.id.alert_layout);
+        final LinearLayout radioLayout = (LinearLayout) rootView.findViewById(R.id.radio_layout);
+        final TextView exclusionAlert = (TextView) rootView.findViewById(R.id.inclusion_alert_one);
+        final TextView exclusionAlert1 = (TextView) rootView.findViewById(R.id.inclusion_alert_two);
+        endSession = (Button) rootView.findViewById(R.id.button_end_session);
+        restartSession = (Button) rootView.findViewById(R.id.button_restart_session);
+        inclusionContent.setText(inclusionContent_array[0]);
         final int[] finalExclusiveContentPosition = {exclusiveContentPosition};
 //        finalExclusiveContentPosition[0]++;
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -43,10 +50,10 @@ View rootView;
                 String yes = "Yes";
                 if (check.equals("Yes")) {
                     finalExclusiveContentPosition[0]++;
-                    if (finalExclusiveContentPosition[0] < exclusiveContent_array.length) {
-                        exclusivePass.setVisibility(View.VISIBLE);
-                        exclusivePass.setText("Passed " + finalExclusiveContentPosition[0] + " of 6");
-                        exclusiveContent.setText(exclusiveContent_array[finalExclusiveContentPosition[0]]);
+                    if (finalExclusiveContentPosition[0] < inclusionContent_array.length) {
+                        inclusionPass.setVisibility(View.VISIBLE);
+                        inclusionPass.setText("Passed " + finalExclusiveContentPosition[0] + " of 6");
+                        inclusionContent.setText(inclusionContent_array[finalExclusiveContentPosition[0]]);
                         radioButton.setChecked(false);
                     }
                     else{
@@ -59,12 +66,37 @@ View rootView;
                 if (check.equals("No")) {
 
 
-                    exclusivePass.setVisibility(View.GONE);
+                    inclusionPass.setVisibility(View.GONE);
+                    radioLayout.setVisibility(View.GONE);
+                    inclusionContent.setVisibility(View.GONE);
+                    alertLayout.setVisibility(View.VISIBLE);
+                    exclusionAlert.setVisibility(View.VISIBLE);
+                    exclusionAlert1.setVisibility(View.VISIBLE);
+                    endSession.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
 
+                            Intent intent = new Intent(getActivity(), ExclusionActivity.class);
+                            getActivity().finish();
+                            System.exit(0);
+                            startActivity(intent);
+
+                        }
+                    });
+
+                    restartSession.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(getActivity(), MainActivity.class);
+                            getActivity().finish();
+                            startActivity(intent);
+                        }
+                    });
 
                 }
             }
         });
+
 
 
         return rootView;
